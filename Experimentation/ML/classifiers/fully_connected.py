2cdf8b2278a6ad1e7ffbbac7d241bef746d1c4c3
Alexandre <alexandre.ayotte2@USherbrooke.ca>
2020-08-08 19:34:51 -0400

    Mutable argument

--- Experimentation/ML/classifiers/fully_connected.py
+++ Experimentation/ML/classifiers/fully_connected.py
@@ -4,13 +4,15 @@ from sklearn.neural_network import MLPClassifier
 from .abstract_classifier import AbstractClassifier
 
 
-
 class FullyConnectedClassifier(AbstractClassifier):
 
-    def __init__(self, hyperparameter = {
-    'hidden_layer_sizes': (10,), 'alpha': 1e-2,
-    'learning_rate_init': 1e-3, 'max_iter': 5000,
-    'batch_size': 50}, dataset=None):
+    def __init__(self, hyperparameter=None, dataset=None):
+
+        if hyperparameter is None:
+            hyperparameter = {
+                'hidden_layer_sizes': (10,), 'alpha': 1e-2,
+                'learning_rate_init': 1e-3, 'max_iter': 5000,
+                'batch_size': 50}
 
         self.hyperparameter = hyperparameter
 
--- Experimentation/ML/classifiers/svm.py
+++ Experimentation/ML/classifiers/svm.py
@@ -6,7 +6,10 @@ from .abstract_classifier import AbstractClassifier
 
 class SVMClassifier(AbstractClassifier):
 
-    def __init__(self, hyperparameter={'C': 1, 'gamma': 1}, dataset=None):
+    def __init__(self, hyperparameter=None, dataset=None):
+
+        if hyperparameter is None:
+            hyperparameter = {'C': 1, 'gamma': 1}
 
         self.hyperparameter = hyperparameter
         super().__init__(SVC(probability=True, gamma=self.hyperparameter['gamma'], C=self.hyperparameter['C']), dataset)
