beb8add8b9502e374ee96ae8490e7b06a4dcfe04
Alexandre <alexandre.ayotte2@USherbrooke.ca>
2020-08-09 18:09:47 -0400

    Breast_cancer experiment created
    The breast_cancer experiment has been extracted from the ML experiment and isolated.

---
+++ Experimentation/Breast_cancer_mlp/breast_cancer_experiment.py
@@ -0,0 +1,52 @@
+from Experimentation.Breast_cancer_mlp.classifiers.fully_connected import FullyConnectedClassifier
+import numpy as np
+from Optimizer.Domain import ContinuousDomain, DiscreteDomain
+from Scheduler.Scheduler import tune_objective
+
+
+def create_objective_func(dataset):
+
+    def objective_func(hparams, device="cpu"):
+        hidden_layer_size = []
+
+        for _ in range(hparams["num_layer"]):
+            hidden_layer_size.append(hparams["layer_size"])
+
+        hyperparameter = {
+            'hidden_layer_sizes': tuple(hidden_layer_size),
+            'learning_rate_init': 10**hparams['lr'],
+            'alpha': 10**hparams['alpha'],
+            'max_iter': 500,
+            'batch_size': int(hparams['b_size'])
+        }
+        net = FullyConnectedClassifier(hyperparameter, dataset)
+
+        net.train()
+
+        return net.evaluate("Testing")
+    return objective_func
+
+
+def run_experiment():
+
+    objective = create_objective_func("breast_cancer")
+
+    h_space = {
+        "lr": ContinuousDomain(-7, -1),
+        "alpha": ContinuousDomain(-7, -1),
+        "b_size": DiscreteDomain(np.arange(50, 500, 10).tolist()),
+        "num_layer": DiscreteDomain(np.arange(1, 20, 1).tolist()),
+        "layer_size": DiscreteDomain(np.arange(5, 550, 1).tolist()),
+    }
+
+    optim_list = ["GP", "GP", "tpe"]
+    acq_list = ["EI", "MPI"]
+    num_iters = 250
+
+    tune_objective(objective_func=objective,
+                   h_space=h_space,
+                   optim_list=optim_list,
+                   acq_func_list=acq_list,
+                   num_iters=num_iters,
+                   save_path="Result/BreastCancer/",
+                   save_each_iter=True)
---
+++ Experimentation/Breast_cancer_mlp/classifiers/abstract_classifier.py
@@ -0,0 +1,39 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from data_loader.data_loader import DataLoader
+
+
+class AbstractClassifier:
+
+    """
+    Parent class of all project classifiers.
+    Attributes:
+        model : An object that defines the classifier classifiers to implement.
+        metrics : An object that defines the different metrics that can be used to evaluate a classifiers.
+        X_train : The features of the training data
+        Y_train : The targets of training data (the ground truth label)
+        X_test :  The features of the testing data
+        Y_test : The targets of training data (the ground truth label)
+    """
+
+    def __init__(self, model, dataset):
+        self.model = model
+        self.data_loader = DataLoader(dataset=dataset)
+        self.X_train = self.data_loader.X_train
+        self.X_test = self.data_loader.X_test
+        self.Y_train = self.data_loader.Y_train
+        self.Y_test = self.data_loader.Y_test
+
+    def train(self):
+        self.model = self.model.fit(self.X_train, self.Y_train)
+
+    def evaluate(self, label="Training"):
+        if label == 'Training':
+            x, y = self.X_train, self.Y_train
+        else:
+            x, y = self.X_test, self.Y_test
+        return 1 - self.model.score(x, y)
+
+
+if __name__ == "__main__":
+    pass
